1ceec4dd3ab0e03df0d79ff684a65f66f5657344
RENO WARNER <reno.warner.777@gmail.com>
2026-05-21 01:49:40 -0400

    FEAT: implement image classification styled interface with ONNX model integration along with samples.

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,6 +1,14 @@
 export default {
 	plugins: {
-		tailwindcss: {},
+		tailwindcss: {
+			content: [
+				"./index.html",
+				"./src/**/*.{js,ts,jsx,tsx}",
+			],
+			theme: {
+				extend: {},
+			},
+		},
 		autoprefixer: {},
 	},
 };
